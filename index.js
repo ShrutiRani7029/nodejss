4b3663e909f3954c4e43c54026a38ee59571d3b1
@coder shruti <www.meshruti@gmail.com>
2024-01-08 19:47:42 +0530

    server is working

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log("hello world");
-//yha hme ise run krne ke liye browser ki need nhi hai ye nodejs ki help se yhi run krdega
-console.log(2+3);
+//ab hm server bnaenge;
+
+//3-steps
+//1.import a package like http
+const http=require('http');
+//2.create a server
+const server=http.createServer((req,res)=>{
+   res.end("<h1>server is working</h1>")
+});
+//3.start the server
+server.listen(5000, ()=>{
+    console.log("server is working");
+});
